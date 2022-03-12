e6064bf16507a5e6ac15d3012148b85dc30dc820
Dean <deank242@gmail.com>
2022-03-11 23:48:45 -0500

    final updates

--- script.js
+++ script.js
@@ -6,71 +6,70 @@ const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+=";
 
-function getLowercase() {
-  return lowerLetters[Math.floor(Math.random() *
-  upperLetters.length)];
-}
-
-function getUppercase() {
-  return upperLetters[Math.floor(Math.random() *
-    upperLetters.length)];
-}
-
-function getNumber() {
-  return numbers[Math.floor(Math.random() *
-    numbers.length)];
-}
-
-function getSymbol() {
- return symbols[Math.floor(Math.random() *
-  symbols.length)];
-}
-
-
-function generatePassword () {
+function generatePassword() {
   var results = "";
-  var promptpasswordsize = window.prompt('How many characters would you like your password?');
+  var promptpasswordsize = window.prompt(
+    "How many characters would you like your password?");
+  var charQty = parseInt(promptpasswordsize);
 
-    if (promptpasswordsize > 7 && promptpasswordsize < 128) {
+  if (charQty > 7 && charQty < 128) {
+    var upperCase = window.confirm("Would you like Upper case letters?");
+    var lowerCase = window.confirm("Would you like Lower case letters?");
+    var numeric = window.confirm("Would you like numbers?");
+    var specialChar = window.confirm("Would you like symbols?");
 
-      var upperLetters = window.confirm("Would you like Upper case letters?");
-      var lowerLetters = window.confirm("Would you like Lower case letters?");
-      var numbers = window.confirm("Would you like numbers?");
-      var symbols = window.confirm("Would you like symbols?");
-      var charQty 
+    var i = 0;
+    while (i < charQty) {
 
-  
-    for (i = 0; i < promptpasswordsize; i++) {
-      results += alphabetUpper[Math.floor(Math.random()* 26)]
-    }
+      if (upperCase == true) {
+        results +=
+          upperLetters[Math.floor(Math.random() * upperLetters.length)];
+        i++;
+        if (i == charQty) {
+          break;
+        }
+      }
+      if (lowerCase == true) {
+        results +=
+          lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+        i++;
+        if (i == charQty) {
+          break;
+        }
+      }
+      if (numeric == true) {
+        results += numbers[Math.floor(Math.random() * numbers.length)];
+        i++;
+        if (i == charQty) {
+          break;
+        }
+      }
+      if (specialChar == true) {
+        results += symbols[Math.floor(Math.random() * symbols.length)];
+        i++;
+        if (i == charQty) {
+          break;
+        }
+      }
+    
   }
-    else {
-      window.alert("That is an invalid entry. Select a length between 8 and 128");
-      return generatePassword();
-    } 
+  } else {
+    window.alert("That is an invalid entry. Select a length between 8 and 128");
+    return generatePassword();
   }
 
-
-
-
+  return results;
+}
 
 // Get references to the #generate element
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
-
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
